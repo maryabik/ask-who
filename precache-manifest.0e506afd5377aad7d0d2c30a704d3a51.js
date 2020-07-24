a45b44abda9a39ae49dc3f5060e3961b11916452
Temilola Abikoye <temilolaabikoye@Temilolas-MacBook-Air.local>
2020-07-24 13:57:23 +0100

    Updates

---
+++ precache-manifest.0e506afd5377aad7d0d2c30a704d3a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042ac335fd1d32e6c44bc780f5425748",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "50d809acc64b8e5e4969",
+    "url": "/monsters-rodolex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47d66af3fa0245260f85",
+    "url": "/monsters-rodolex/static/js/2.96419cc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rodolex/static/js/2.96419cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d809acc64b8e5e4969",
+    "url": "/monsters-rodolex/static/js/main.edd1f422.chunk.js"
+  },
+  {
+    "revision": "b0fa247538924f480bb9",
+    "url": "/monsters-rodolex/static/js/runtime-main.613e3a2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rodolex/static/media/logo.5d5d9eef.svg"
+  }
+]);
